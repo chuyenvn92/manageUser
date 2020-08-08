5ec9f9b9218650c65ee3fd7a90877bae16753111
chuyen <maicongchuyentb@gmail.com>
2020-08-08 11:05:04 +0700

    Updates

---
+++ precache-manifest.d27167da76daf6156897b2da35a71748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c388241dcf2ab2286af791cba89196b9",
+    "url": "/manageUser/index.html"
+  },
+  {
+    "revision": "fdac87f9838c4323dc65",
+    "url": "/manageUser/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "939cef955cc9f0493bc2",
+    "url": "/manageUser/static/js/2.75fe50c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manageUser/static/js/2.75fe50c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdac87f9838c4323dc65",
+    "url": "/manageUser/static/js/main.ecea8645.chunk.js"
+  },
+  {
+    "revision": "872cbf22f536a25c57a9",
+    "url": "/manageUser/static/js/runtime-main.99e882b0.js"
+  }
+]);
